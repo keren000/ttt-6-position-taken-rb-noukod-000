2bd3773065134e15f631589d72d844fc37ecd232
Keren-Happuc Fleurimond <keren@noukod.com>
2018-08-31 17:49:41 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,18 @@
 # code your #position_taken? method here!
+def position_taken?(arr, index)
+  if arr[index] == "X" || arr[index] == "O"
+    return true
+  else arr[index] == " " || arr[index] == "" || arr[index] == nil
+    return false
+  end
+end
+
+def valid_move?(arr, index)
+  if !position_taken?(arr, index) && index.between?(0, 10)
+    return true
+  else
+    return false
+  end
+end
+
+
